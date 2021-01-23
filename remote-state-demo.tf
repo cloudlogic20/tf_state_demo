e75678fd8e2241ad44538de99c49a017012b7731
Cloud Logic <cloudlogic20@gmail.com>
2021-01-23 08:22:19 +0530

    creds managed

--- remote-state-demo.tf
+++ remote-state-demo.tf
@@ -7,7 +7,7 @@ resource "aws_db_instance" "default" {
   instance_class       = "db.t2.micro"
   name                 = "mydb"
   username             = "foo"
-  password             = "mysecretpassword"
+  password             = file("${path.module}/confidential.txt")
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = "true"
 }
